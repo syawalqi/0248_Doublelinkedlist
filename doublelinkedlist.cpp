ab47b96a918b7223a355916488bb8b4990a6d1b4
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-02 15:39:05 +0700

    membuat node start dan void addNote

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -11,3 +11,13 @@ struct node
 
 };
 
+node* start = NULL;
+
+void addNode() {
+    node* newNode = new node(); //step1: create A NEW NODE
+    cout << "\nEnter the roll number of the student:";
+    cin >> newNode -> noMhs; // assign value to the data field of the new Node 
+    cout << "\nEnter the name of the student:";
+    cin >> newNode -> name; // assign value to the data field of the new Node 
+}
+
