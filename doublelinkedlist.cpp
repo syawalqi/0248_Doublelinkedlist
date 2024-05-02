c1467c8eb3f1a96ace7a548bfab831d334eb7117
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-02 15:52:00 +0700

    membuat if else dari newnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -19,5 +19,24 @@ void addNode() {
     cin >> newNode -> noMhs; // assign value to the data field of the new Node 
     cout << "\nEnter the name of the student:";
     cin >> newNode -> name; // assign value to the data field of the new Node 
+
+
+    //insert the nnew node in the list
+    if (start == NULL || newNode->noMhs <= start->noMhs) {
+
+        if (start != NULL && newNode->noMhs == start->noMhs) {
+            cout << "\033[31mDuplicate roll numbers are not allowed\033[0m" <<endl;
+            return;
+        }
+        newNode->next = start; 
+        if (start != NULL) {
+            start-> prev = newNode;
+        }
+        newNode->prev = NULL; 
+        start = newNode;
+    }
+    else{
+        
+    }
 }
 
